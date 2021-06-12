af8e722001dfb5ded88c0387877ec81d3cd2bc29
Nikita Parfenov <n1koss.boy@gmail.com>
2021-06-12 19:58:40 +0500

    Add last task number 1

--- light/1-task.js
+++ light/1-task.js
@@ -7,7 +7,7 @@
  * вернуть “1-4,6-7,9,11"
  */
 
-const compareArray = (numbers) => {
+const collapseNumbers = (numbers) => {
   numbers = numbers.sort((a, b) => a - b);
   const chunks = [];
 
@@ -15,16 +15,21 @@ const compareArray = (numbers) => {
   numbers.forEach((num) => {
     chunk.push(num);
 
-    if (!numbers.find((n) => n === num + 1)) {
+    const nextNum = numbers.find((n) => n === num + 1);
+    if (!nextNum) {
       chunks.push(chunk);
       chunk = [];
     }
   });
 
   return chunks
-    .map((c) => (c.length === 1 ? c[0] : `${c[0]}-${c[c.length - 1]}`))
+    .map((chunk) =>
+      chunk.length === 1
+        ? chunk[0]
+        : `${chunk[0]}-${chunk[chunk.length - 1]}`
+    )
     .join(', ');
 };
 
-const answer = compareArray([6, 1, 3, 9, 2, 4, 11, 7]);
+const answer = collapseNumbers([6, 1, 3, 9, 2, 4, 11, 7]);
 console.log('answer: ', answer);
