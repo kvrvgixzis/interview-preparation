c5b99f245e9178b23467dcdfc5b57ece45a62f17
Nikita Parfenov <nktpfn@doubletapp.ai>
2021-06-12 18:48:23 +0500

    Add get-function.js

---
+++ get-function.js
@@ -0,0 +1,27 @@
+/**
+ * Необходимо написать функцию, которая на вход принимает урл,
+ * асинхронно ходит по этому урлу GET запросом и возвращает данные (json)
+ * Для получения данных можно использовать $.get или fetch.
+ * Если во время запроса произошла ошибка, то пробовать запросить еще 5 раз.
+ * Если в итоге информацию получить не удалось, вернуть ошибку "Заданный URL недоступен".
+ */
+
+import fetch from 'node-fetch';
+
+const get = async (url, limit = 5) => {
+  try {
+    console.log('Start fetching');
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    if (limit === 1) throw 'Заданный URL недоступен';
+    return await get(url, limit - 1);
+  }
+};
+
+const url = 'https://rickandmortyai.com/api/character/483';
+
+get(url)
+  .then((res) => console.log('res: ', res))
+  .catch((err) => console.error('err: ', err));
