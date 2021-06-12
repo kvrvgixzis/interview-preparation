80d79ab88ed0822616cd6fe2681d5ef76af74db4
Nikita Parfenov <n1koss.boy@gmail.com>
2021-06-12 23:18:58 +0500

    Refactor tests

--- tasks/reducer.js
+++ tasks/reducer.js
@@ -18,14 +18,3 @@ export const reducer = (action) => {
       break;
   }
 };
-
-reducer({ type: 'ADD', payload: { id: 1, name: 'Anton' } });
-reducer({ type: 'ADD', payload: { id: 2, name: 'Kirill' } });
-reducer({ type: 'ADD', payload: { id: 3, name: 'Natasha' } });
-reducer({ type: 'DEL', payload: 2 });
-reducer({ type: 'FIND', payload: 1 });
-reducer({ type: 'ADD', payload: { id: 4, name: 'Kirill2' } });
-reducer({ type: 'ADD', payload: { id: 5, name: 'Natasha32' } });
-reducer({ type: 'FIND', payload: 6 });
-reducer({ type: 'FIND', payload: 2 });
-reducer({ type: 'FIND', payload: 5 });
