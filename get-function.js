2ffbe127c5092a17d420a6e7fefcfe66871c7b78
Nikita Parfenov <n1koss.boy@gmail.com>
2021-06-12 18:57:56 +0500

    Add broken url to get-function.js

--- get-function.js
+++ get-function.js
@@ -1,16 +1,8 @@
-/**
- * Необходимо написать функцию, которая на вход принимает урл,
- * асинхронно ходит по этому урлу GET запросом и возвращает данные (json)
- * Для получения данных можно использовать $.get или fetch.
- * Если во время запроса произошла ошибка, то пробовать запросить еще 5 раз.
- * Если в итоге информацию получить не удалось, вернуть ошибку "Заданный URL недоступен".
- */
-
 import fetch from 'node-fetch';
 
 const get = async (url, limit = 5) => {
   try {
-    console.log('Start fetching');
+    console.log(`Try fetching ${url}`);
     const response = await fetch(url);
     const json = await response.json();
     return json;
@@ -21,7 +13,7 @@ const get = async (url, limit = 5) => {
 };
 
 const url = 'https://rickandmortyapi.com/api/character/483';
-const brokenUrl = 'https://rickandmortpi.com/api/character/483';
+const brokenUrl = 'broken';
 
 get(url)
   .then((res) => console.log('res: ', res))
