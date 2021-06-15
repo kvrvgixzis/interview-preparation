d6c836d0a124b29a37fc8b889afd0a22cb7dfab7
Nikita Parfenov <n1koss.boy@gmail.com>
2021-06-15 12:31:52 +0500

    Implement subarray sum task

---
+++ tasks/subarray-sum.js
@@ -0,0 +1,21 @@
+/** Given an array of integers nums
+ * and an integer k, return the total
+ * number of continuous subarrays
+ * whose sum equals to k. */
+
+export const subarraySum = (nums, k) => {
+  let count = 0;
+  let prefixSum = new prefixSum();
+  prefixSum.set(0, 1);
+
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    let target = sum - k;
+    if (prefixSum.has(target)) count += prefixSum.get(target);
+    if (!prefixSum.has(sum)) prefixSum.set(sum, 0);
+    prefixSum.set(sum, prefixSum.get(sum) + 1);
+  }
+
+  return count;
+};
